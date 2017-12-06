ee67da8da6e7e87954ccc6a8272f627a78019c42
Ben Verbeken <ben@ostia.be>
2017-12-06 10:50:16 +0100

    added test utility loader

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,3 +1,38 @@
 <?php
-// Setup autoloading
-require dirname(__FILE__) . '/../vendor/autoload.php';
+
+loadLibs();
+loadTestUtilities();
+
+
+function loadTestUtilities()
+{
+    loadDirectoryContents(dirname(__FILE__) . '/util');
+}
+
+function loadDirectoryContents($directoryPath)
+{
+    $directoryContents = getDirContents($directoryPath);
+    foreach ($directoryContents as $filename) {
+        require $filename;
+    }
+}
+
+function getDirContents($dir, &$results = array())
+{
+    $files = scandir($dir);
+    foreach ($files as $key => $value) {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+        if (is_file($path)) {
+            $results[] = $path;
+        } else if (is_dir($path) && $value != "." && $value != "..") {
+            getDirContents($path, $results);
+        }
+    }
+
+    return $results;
+}
+
+function loadLibs()
+{
+    require dirname(__FILE__) . '/../vendor/autoload.php';
+}
