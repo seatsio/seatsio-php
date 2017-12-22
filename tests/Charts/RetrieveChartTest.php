0969258d20bb87e2c0a51f678d9f94170c3990b1
mroloux <matti@seats.io>
2017-12-22 09:37:19 +0100

    Added test for retrieve chart call

---
+++ tests/Charts/RetrieveChartTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Seatsio\Charts;
+
+use Seatsio\SeatsioClientTest;
+
+class RetrieveChartTest extends SeatsioClientTest
+{
+
+    public function test()
+    {
+        $chart = $this->seatsioClient->charts()->create();
+        $this->seatsioClient->charts()->addTag($chart->key, "tag1");
+
+        $retrievedChart = $this->seatsioClient->charts()->retrieve($chart->key);
+
+        self::assertEquals($chart->key, $retrievedChart->key);
+        self::assertNotNull($retrievedChart->id);
+        self::assertEquals('Untitled chart', $retrievedChart->name);
+        self::assertEquals('NOT_USED', $retrievedChart->status);
+        self::assertNotNull($retrievedChart->publishedVersionThumbnailUrl);
+        self::assertNull($retrievedChart->draftVersionThumbnailUrl);
+        self::assertEquals(['tag1'], $retrievedChart->tags);
+    }
+
+}
