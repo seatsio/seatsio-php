e5a5f5d7545df3634a392c273e53c80032d7c068
mroloux <matti@seats.io>
2020-10-08 13:14:59 +0200

    Added fixedGroupLayout, maxOccupancyAbsolute nad maxOccupancyPercentage to SocialDistancingRuleset

--- src/Charts/SocialDistancingRuleset.php
+++ src/Charts/SocialDistancingRuleset.php
@@ -35,6 +35,21 @@ class SocialDistancingRuleset
     public $maxGroupSize;
 
     /**
+     * @var int
+     */
+    public $maxOccupancyAbsolute;
+
+    /**
+     * @var int
+     */
+    public $maxOccupancyPercentage;
+
+    /**
+     * @var boolean
+     */
+    public $fixedGroupLayout;
+
+    /**
      * @var array[string]
      */
     public $disabledSeats;
@@ -44,7 +59,7 @@ class SocialDistancingRuleset
      */
     public $enabledSeats;
 
-    public function __construct($index = 0, $name = null, $numberOfDisabledSeatsToTheSides = 0, $disableSeatsInFrontAndBehind = false, $numberOfDisabledAisleSeats = 0, $maxGroupSize = 0, $disabledSeats = [], $enabledSeats = [])
+    public function __construct($index = 0, $name = null, $numberOfDisabledSeatsToTheSides = 0, $disableSeatsInFrontAndBehind = false, $numberOfDisabledAisleSeats = 0, $maxGroupSize = 0, $maxOccupancyAbsolute = 0, $maxOccupancyPercentage = 0, $fixedGroupLayout = false, $disabledSeats = [], $enabledSeats = [])
     {
         $this->index = $index;
         $this->name = $name;
@@ -52,7 +67,19 @@ class SocialDistancingRuleset
         $this->disableSeatsInFrontAndBehind = $disableSeatsInFrontAndBehind;
         $this->numberOfDisabledAisleSeats = $numberOfDisabledAisleSeats;
         $this->maxGroupSize = $maxGroupSize;
+        $this->maxOccupancyAbsolute = $maxOccupancyAbsolute;
+        $this->maxOccupancyPercentage = $maxOccupancyPercentage;
+        $this->fixedGroupLayout = $fixedGroupLayout;
         $this->disabledSeats = $disabledSeats;
         $this->enabledSeats = $enabledSeats;
+    }
+
+    public static function fixed($index = 0, $name = null, $disabledSeats = []) {
+        return new SocialDistancingRuleset($index, $name, 0, false, 0, 0, 0, 0, true, $disabledSeats, []);
+    }
+
+    public static function ruleBased($index = 0, $name = null, $numberOfDisabledSeatsToTheSides = 0, $disableSeatsInFrontAndBehind = false, $numberOfDisabledAisleSeats = 0, $maxGroupSize = 0, $maxOccupancyAbsolute = 0, $maxOccupancyPercentage = 0, $disabledSeats = [], $enabledSeats = []) {
+        return new SocialDistancingRuleset($index, $name, $numberOfDisabledSeatsToTheSides, $disableSeatsInFrontAndBehind, $numberOfDisabledAisleSeats, $maxGroupSize,
+            $maxOccupancyAbsolute, $maxOccupancyPercentage, false, $disabledSeats, $enabledSeats);
     }
 }
--- tests/Charts/SaveSocialDistancingRulesetsTest.php
+++ tests/Charts/SaveSocialDistancingRulesetsTest.php
@@ -12,11 +12,41 @@ class SaveSocialDistancingRulesetsTest extends SeatsioClientTest
         $chart = $this->seatsioClient->charts->create();
 
         $rulesets = [
-            "ruleset1" => new SocialDistancingRuleset(0, "My first ruleset", 1, true, 2, 1, ["A-1"], ["A-2"]),
+            "ruleset1" => new SocialDistancingRuleset(0, "My first ruleset", 1, true, 2, 1, 10, 0, false, ["A-1"], ["A-2"]),
             "ruleset2" => new SocialDistancingRuleset(1, "My second ruleset")
         ];
         $this->seatsioClient->charts->saveSocialDistancingRulesets($chart->key, $rulesets);
 
+        $retrievedChart = $this->seatsioClient->charts->retrieve($chart->key);
+        self::assertEquals($rulesets, $retrievedChart->socialDistancingRulesets);
+    }
+
+    public function testFixed()
+    {
+        $chart = $this->seatsioClient->charts->create();
+
+        $this->seatsioClient->charts->saveSocialDistancingRulesets($chart->key, [
+            "ruleset1" => SocialDistancingRuleset::fixed(0, "My first ruleset", ["A-1"])
+        ]);
+
+        $rulesets = [
+            "ruleset1" => new SocialDistancingRuleset(0, "My first ruleset", 0, false, 0, 0, 0, 0, true, ["A-1"], []),
+        ];
+        $retrievedChart = $this->seatsioClient->charts->retrieve($chart->key);
+        self::assertEquals($rulesets, $retrievedChart->socialDistancingRulesets);
+    }
+
+    public function testRuleBased()
+    {
+        $chart = $this->seatsioClient->charts->create();
+
+        $this->seatsioClient->charts->saveSocialDistancingRulesets($chart->key, [
+            "ruleset1" => SocialDistancingRuleset::ruleBased(0, "My first ruleset", 1, true, 2, 1, 10, 0, ["A-1"], ["A-2"])
+        ]);
+
+        $rulesets = [
+            "ruleset1" => new SocialDistancingRuleset(0, "My first ruleset", 1, true, 2, 1, 10, 0, false, ["A-1"], ["A-2"]),
+        ];
         $retrievedChart = $this->seatsioClient->charts->retrieve($chart->key);
         self::assertEquals($rulesets, $retrievedChart->socialDistancingRulesets);
     }
