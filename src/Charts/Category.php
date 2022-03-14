8b0fdbbbba00ea17abbf10177a66f402ea2d638d
Ben Verbeken <ben@seats.io>
2022-03-14 11:48:32 +0100

    refactoring: clean separation between Category (what the server returns) and CategoryRequestBuilder (what the client sends)

--- src/Charts/Category.php
+++ src/Charts/Category.php
@@ -19,30 +19,6 @@ class Category
     /**
      * @var bool
      */
-    public $accessible = false;
+    public $accessible;
 
-
-    public function setKey($key): self
-    {
-        $this->key = $key;
-        return $this;
-    }
-
-    public function setLabel($label): self
-    {
-        $this->label = $label;
-        return $this;
-    }
-
-    public function setColor($color): self
-    {
-        $this->color = $color;
-        return $this;
-    }
-
-    public function setAccessible($accessible): self
-    {
-        $this->accessible = $accessible;
-        return $this;
-    }
 }
---
+++ src/Charts/CategoryRequestBuilder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Seatsio\Charts;
+
+class CategoryRequestBuilder
+{
+
+
+    /**
+     * @param $key int|string
+     */
+    public function setKey($key): self
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+    public function setAccessible(bool $accessible): self
+    {
+        $this->accessible = $accessible;
+        return $this;
+    }
+}
