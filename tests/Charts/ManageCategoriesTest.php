bfc39f7f994eca633cf5bf3e6a15da844d8e6e3c
Ben Verbeken <ben@seats.io>
2022-03-18 17:22:44 +0100

    ability to add & remove a chart category

---
+++ tests/Charts/ManageCategoriesTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Seatsio\Charts;
+
+use Seatsio\SeatsioClientTest;
+
+class ManageCategoriesTest extends SeatsioClientTest
+{
+
+    public function testAddCategory()
+    {
+        $categories = [
+            (object)['key' => 1, 'label' => 'Category 1', 'color' => '#aaaaaa', 'accessible' => true]
+        ];
+        $chart = $this->seatsioClient->charts->create('aChart', null, $categories);
+
+        $category2 = (new CategoryRequestBuilder())->setKey(2)->setLabel('Category 2')->setColor('blue');
+        $this->seatsioClient->charts->addCategory($chart->key, $category2);
+
+        $retrievedChart = $this->seatsioClient->charts->retrievePublishedVersion($chart->key);
+        self::assertEquals('aChart', $retrievedChart->name);
+        self::assertEquals([
+            (object)['key' => 1, 'label' => 'Category 1', 'color' => '#aaaaaa', 'accessible' => true],
+            (object)['key' => 2, 'label' => 'Category 2', 'color' => 'blue', 'accessible' => false]
+        ], $retrievedChart->categories->list);
+    }
+
+    public function testRemoveCategory()
+    {
+        $categories = [
+            (object)['key' => 1, 'label' => 'Category 1', 'color' => '#aaaaaa', 'accessible' => true],
+            (object)['key' => 'cat2', 'label' => 'Category 2', 'color' => '#bbbbbb', 'accessible' => false]
+        ];
+        $chart = $this->seatsioClient->charts->create('aChart', null, $categories);
+
+        $this->seatsioClient->charts->removeCategory($chart->key, 1);
+
+        $retrievedChart = $this->seatsioClient->charts->retrievePublishedVersion($chart->key);
+        self::assertEquals('aChart', $retrievedChart->name);
+        self::assertEquals([
+            (object)['key' => 'cat2', 'label' => 'Category 2', 'color' => '#bbbbbb', 'accessible' => false]
+        ], $retrievedChart->categories->list);
+    }
+
+}
