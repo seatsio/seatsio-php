0a7c9ccda6a25df4dd3d8d36cbf5839a129c54bd
Ben Verbeken <ben@seats.io>
2022-07-07 11:02:07 +0200

    resolved 'Return type declaration must be compatible with parent'

--- src/PagedIterator.php
+++ src/PagedIterator.php
@@ -25,17 +25,17 @@ class PagedIterator implements Iterator
         $this->params = $params;
     }
 
-    public function current()
+    public function current(): mixed
     {
         return $this->getCurrentPage()->items[$this->indexInCurrentPage];
     }
 
-    public function next()
+    public function next(): void
     {
         $this->indexInCurrentPage++;
     }
 
-    public function key()
+    public function key(): mixed
     {
         return $this->current()->id;
     }
