85dfb1575915421638ab248d2b0a5f7826c05178
Matti Roloux <matti@seats.io>
2023-06-26 09:46:00 +0200

    Prevent accidental releases

--- release.mjs
+++ release.mjs
@@ -20,7 +20,7 @@
 $.verbose = false
 
 const versionToBump = getVersionToBump()
-const latestVersion = await fetchLatestReleasedVersionNumber()
+const latestReleaseTag = await fetchLatestReleasedVersionNumber()
 
 await pullLastVersion().then(release)
 
@@ -40,8 +40,7 @@ function removeLeadingV(tagName) {
 
 async function fetchLatestReleasedVersionNumber() {
     let result = await $`gh release view --json tagName`
-    let tagName = JSON.parse(result).tagName
-    return removeLeadingV(tagName)
+    return JSON.parse(result).tagName
 }
 
 async function determineNextVersionNumber(previous) {
@@ -53,8 +52,25 @@ async function pullLastVersion() {
     await $`git pull origin master`
 }
 
+async function getCurrentCommitHash() {
+    return (await $`git rev-parse HEAD`).stdout.trim()
+}
+
+async function getCommitHashOfTag(tag) {
+    return (await $`git rev-list -n 1 ${tag}`).stdout.trim()
+}
+
+async function assertChangesSinceRelease(releaseTag) {
+    let masterCommitHash = await getCurrentCommitHash()
+    let latestReleaseCommitHash = await getCommitHashOfTag(releaseTag)
+    if(masterCommitHash === latestReleaseCommitHash) {
+        throw new Error("No changes on master since release tagged " + releaseTag)
+    }
+}
+
 async function release() {
-    const nextVersion = await determineNextVersionNumber(latestVersion)
+    await assertChangesSinceRelease(latestReleaseTag)
+    const nextVersion = await determineNextVersionNumber(removeLeadingV(latestReleaseTag))
     const newTag = 'v' + nextVersion
     return await $`gh release create ${newTag} --generate-notes`.catch(error => {
         console.error('something went wrong while creating the release. Please revert the version change!')
