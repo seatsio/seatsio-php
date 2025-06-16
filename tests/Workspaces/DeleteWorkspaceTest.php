3e2643e288f057fe61c1adc07137f537b15e269e
Steve Chaloner <steve@seats.io>
2025-06-16 17:04:29 +0200

    Delete workspaces
    This PR exposes workspace deletion for inactive workspaces.

---
+++ tests/Workspaces/DeleteWorkspaceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Seatsio\Charts;
+
+use Seatsio\SeatsioClientTest;
+use Seatsio\SeatsioException;
+
+class DeleteWorkspaceTest extends SeatsioClientTest
+{
+    public function testDeleteInactiveWorkspace()
+    {
+        $workspace = $this->seatsioClient->workspaces->create("a ws");
+        $this->seatsioClient->workspaces->deactivate($workspace->key);
+
+        $this->seatsioClient->workspaces->delete($workspace->key);
+
+        try {
+            $this->seatsioClient->workspaces->retrieve($workspace->key);
+            throw new \Exception("Should have failed");
+        } catch (SeatsioException $exception) {
+            self::assertEquals(1, sizeof($exception->errors));
+            self::assertEquals("No workspace found with public key '" . $workspace->key . "'", $exception->errors[0]->message);
+        }
+    }
+
+    public function testDeleteActiveWorkspace()
+    {
+        $workspace = $this->seatsioClient->workspaces->create("a ws");
+
+        try {
+            $this->seatsioClient->workspaces->delete($workspace->key);
+            throw new \Exception("Should have failed");
+        } catch (SeatsioException $exception) {
+            self::assertEquals(1, sizeof($exception->errors));
+            self::assertEquals("CANNOT_DELETE_ACTIVE_WORKSPACE", $exception->errors[0]->code);
+            self::assertEquals("Cannot delete active workspace, please deactivate it first", $exception->errors[0]->message);
+        }
+    }
+}
