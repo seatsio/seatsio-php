8367de32768704d44a5e0d8638f2e1b2de097b2c
Steve Chaloner <steve@seats.io>
2025-06-17 13:20:02 +0200

    Removed unnecessary test

--- tests/Workspaces/DeleteWorkspaceTest.php
+++ tests/Workspaces/DeleteWorkspaceTest.php
@@ -22,18 +22,4 @@ class DeleteWorkspaceTest extends SeatsioClientTest
             self::assertEquals("No workspace found with public key '" . $workspace->key . "'", $exception->errors[0]->message);
         }
     }
-
-    public function testDeleteActiveWorkspace()
-    {
-        $workspace = $this->seatsioClient->workspaces->create("a ws");
-
-        try {
-            $this->seatsioClient->workspaces->delete($workspace->key);
-            throw new \Exception("Should have failed");
-        } catch (SeatsioException $exception) {
-            self::assertEquals(1, sizeof($exception->errors));
-            self::assertEquals("CANNOT_DELETE_ACTIVE_WORKSPACE", $exception->errors[0]->code);
-            self::assertEquals("Cannot delete active workspace, please deactivate it first", $exception->errors[0]->message);
-        }
-    }
 }
