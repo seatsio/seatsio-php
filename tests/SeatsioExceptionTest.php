88a4eb1231b1b824639c160f92a465a35bf8be3b
Scott Divelbiss <scottdivelbiss@gmail.com>
2026-01-20 16:28:37 +0100

    testTimeoutThrowsSeatsioException

--- tests/SeatsioExceptionTest.php
+++ tests/SeatsioExceptionTest.php
@@ -2,6 +2,10 @@
 
 namespace Seatsio;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 
@@ -22,5 +26,24 @@ class SeatsioExceptionTest extends SeatsioClientTest
         $exception = SeatsioException::from($request, $response);
         self::assertNull($exception->requestId);
         self::assertStringStartsWith("GET http://dummy.uri resulted in a `400 Bad Request` response.", $exception->getMessage());
+    }
+
+    public function testTimeoutThrowsSeatsioException()
+    {
+        $this->expectException(SeatsioException::class);
+        $this->expectExceptionMessage("Connection timed out");
+
+        $request = new Request("GET", "http://dummy.uri");
+        $mock = new MockHandler([
+            new ConnectException("Connection timed out", $request)
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client([
+            'handler' => $handlerStack,
+            'timeout' => 0.01
+        ]);
+
+        $charts = new Charts\Charts($client);
+        $charts->listAll();
     }
 }
