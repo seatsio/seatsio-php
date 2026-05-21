53c33dd513dfcd6fce418acff12cca87618c423c
mroloux <matti@seats.io>
2026-05-21 12:53:35 +0200

    Updated tests

--- tests/Events/Channels/ReplaceChannelsTest.php
+++ tests/Events/Channels/ReplaceChannelsTest.php
@@ -14,14 +14,14 @@ class ReplaceChannelsTest extends SeatsioClientTest
         $event = $this->seatsioClient->events->create($chartKey);
 
         $this->seatsioClient->events->channels->replace($event->key, [
-            new Channel("channelKey1", "channel 1", "#FF0000", 1, ["A-1", "A-2"]),
+            new Channel("channelKey1", "channel 1", "#FF0000", 1, ["A-1", "A-2"], ["GA1" => 3]),
             new Channel("channelKey2", "channel 2", "#00FFFF", 2, [])
         ]);
 
         $retrievedEvent = $this->seatsioClient->events->retrieve($event->key);
 
         self::assertEquals([
-            new Channel("channelKey1", "channel 1", "#FF0000", 1, ["A-1", "A-2"]),
+            new Channel("channelKey1", "channel 1", "#FF0000", 1, ["A-1", "A-2"], ["GA1" => 3]),
             new Channel("channelKey2", "channel 2", "#00FFFF", 2, [])
         ], $retrievedEvent->channels);
     }
