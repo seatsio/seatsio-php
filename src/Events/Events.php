139e7a3d1566a56f9fe0411c26247ef1e53d29ac
mroloux <matti@seats.io>
2026-09-11 13:00:13 +0200

    Restored unrelated changes, removed test

--- src/Events/Events.php
+++ src/Events/Events.php
@@ -640,5 +640,30 @@ class Events
         return $mapper->map($json, new BestAvailableObjects());
     }
 
-}
+    public function moveEventToNewChartCopy(string $eventKey): Event
+    {
+        $res = $this->client->post(UriTemplate::expand('/events/{key}/actions/move-to-new-chart-copy', array("key" => $eventKey)));
+        $json = GuzzleResponseDecoder::decodeToObject($res);
+        $mapper = SeatsioJsonMapper::create();
+        return $mapper->map($json, new Event());
+    }
 
+    private static function normalizeObjects($objectOrObjects): array
+    {
+        if (is_array($objectOrObjects)) {
+            if (count($objectOrObjects) === 0) {
+                return [];
+            }
+            return array_map(function ($object) {
+                if ($object instanceof ObjectProperties) {
+                    return $object->toArray();
+                }
+                if (is_string($object)) {
+                    return ["objectId" => $object];
+                }
+                return $object;
+            }, $objectOrObjects);
+        }
+        return self::normalizeObjects([$objectOrObjects]);
+    }
+}
